7d893ccf7ceda69c5d9e5e1707803adc5389cc70
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-07 19:58:49 +0530

    Added:calculated Employee wage using Switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,29 +3,32 @@ package employeewage;
 public class EmployeeWage {
 	public static final int IS_FULL_PRESENT = 1;
 	public static final int IS_PART__PRESENT = 2;
-	public static void employeeAttendance() {
-		System.out.println("Welcome to Employee Wage Problem using oops");
+
+	public static void employeeSalary() {
 		int wage_per_hr = 20;
 		int full_day_work_hr = 8;
 		int part_day_work_hr = 4;
 		int salary;
 		double random = Math.floor(Math.random() * 10) % 3;
-		System.out.println("random value is:" + random);
-		if (random == IS_FULL_PRESENT) {
+		System.out.println("random variable is:" + random);
+		switch ((int) random) {
+		case 1:
 			System.out.println("employee is present");
 			salary = (wage_per_hr * full_day_work_hr);
 			System.out.println("salary is:" + salary);
-		} else if (random == IS_PART__PRESENT) {
+			break;
+		case 2:
 			salary = (wage_per_hr * part_day_work_hr);
 			System.out.println("salary is :" + salary);
 			System.out.println("employee is part time present");
-		} else {
+			break;
+		default:
 			System.out.println("when employee is absent for the day his salary is zero");
 		}
 	}
 
 	public static void main(String[] args) {
 		EmployeeWage employeewage = new EmployeeWage();
-		employeewage.employeeAttendance();
+		employeewage.employeeSalary();
 	}
 }
