6fc352f5eeff5a3af3eae91c4070d54a2ba07acc
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-07 20:32:56 +0530

    Computing Employee Wage for multiple companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,42 +6,38 @@ public class EmployeeWage {
 	public static final int IS_FULL_PRESENT = 1;
 	public static final int IS_PART__PRESENT = 2;
 
-	public static void employeeSalaryPerMonth() {
+	public static void employeeSalaryPerMonth(String company, int wagePerHr, int workingDays) {
 		System.out.println("Welcome to Employee Wage Problem using oops");
-		int wage_per_hr=20;
-		int fullDayWorkHr=8; 
-		int partDayWorkHr=4; 
-		int salary;
-		int workingDays=20;
-		int salaryPerMonth=0;
-		int totalWorkingHrs=0;
-		int i;
-		  for (i=0;i<workingDays;i++)
-		  {
-				Random var = new Random();
-				int random;
-				random=var.nextInt(3);
-				switch(random) {
-				case 1:
-					salary=(wage_per_hr*fullDayWorkHr);
-					salaryPerMonth=(salaryPerMonth+salary);
-					totalWorkingHrs=(totalWorkingHrs+fullDayWorkHr);
-					break;
-				case 2:
-					salary=(wage_per_hr*partDayWorkHr);
-					salaryPerMonth=(salaryPerMonth+salary);
-					totalWorkingHrs=(totalWorkingHrs+partDayWorkHr);
-					break;
-			  }
-				if (totalWorkingHrs==100 || i==20)
-				{
-					break;
-				}
+		int fullDayWorkHr = 8, partDayWorkHr = 4, salary;
+		int salaryPerMonth = 0, totalWorkingHrs = 0, i;
+		for (i = 0; i < workingDays; i++) {
+			Random var = new Random();
+			int random;
+			random = var.nextInt(3);
+			switch (random) {
+			case 1:
+				salary = (wagePerHr * fullDayWorkHr);
+				salaryPerMonth = (salaryPerMonth + salary);
+				totalWorkingHrs = (totalWorkingHrs + fullDayWorkHr);
+				break;
+			case 2:
+				salary = (wagePerHr * partDayWorkHr);
+				salaryPerMonth = (salaryPerMonth + salary);
+				totalWorkingHrs = (totalWorkingHrs + partDayWorkHr);
+				break;
 			}
-		System.out.println("salary is credited for" +totalWorkingHrs+ "hours"  +i+ "days"+salaryPerMonth);			
-}
+			if (totalWorkingHrs == 100 || i == 20) {
+				break;
+			}
+		}
+		System.out.println("salary is credited for" + totalWorkingHrs + "hours" + i + "days for a employee in "
+				+ company + " is" + salaryPerMonth);
+	}
 
 	public static void main(String[] args) {
-		EmployeeWage.employeeSalaryPerMonth();
+		EmployeeWage employeewage = new EmployeeWage();
+		EmployeeWage employeewagetwo = new EmployeeWage();
+		employeewage.employeeSalaryPerMonth("Infosys", 8, 20);
+		employeewage.employeeSalaryPerMonth("Hp", 10, 22);
 	}
 }
