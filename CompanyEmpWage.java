741b5da91e07b2d80da49721620dd7944666454e
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-09 20:51:19 +0530

    Refactor:code to manage Employee wage for multiple companies

---
+++ CompanyEmpWage.java
@@ -0,0 +1,26 @@
+package empcode;
+
+public class CompanyEmpWage {
+    public final String company;
+    public final int empRatePerHour;
+    public final int numOfWorkingDays;
+    public final int maxHoursPerMonth;
+    public int totalEmpWage;
+
+    public CompanyEmpWage(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth) {
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+        this.totalEmpWage = 0;
+    }
+
+    public void setTotalEmpWage(int totalEmpwage) {
+        this.totalEmpWage = totalEmpWage;
+    }
+
+    @Override
+    public String toString() {
+        return "Total Emp Wage for Company: "+company+" is: "+ totalEmpWage;
+    }
+}
